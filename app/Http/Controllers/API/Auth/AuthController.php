e977be179003d9ab29b3ee99045b4e825b9e5b26
abraham garcia <adolfo.91.garcia@gmail.com>
2021-03-26 15:09:06 -0300

    primer test

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -8,4 +8,20 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:55',
+            'email' => 'email|required|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $validatedData['password'] = bcrypt($request->password);
+
+        $user = User::create($validatedData);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response([ 'user' => $user, 'access_token' => $accessToken, 'message' => 'Register successfully'], 201);
+    }
 }
